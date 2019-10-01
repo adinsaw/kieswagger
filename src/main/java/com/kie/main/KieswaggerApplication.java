b5240017c32a5531f08cad8d072916ae343d5f27
adinsaw <adinsaw@gmail.com>
2019-10-01 07:17:15 -0600

    Add files via upload

---
+++ src/main/java/com/kie/main/KieswaggerApplication.java
@@ -0,0 +1,13 @@
+package com.kie.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieswaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieswaggerApplication.class, args);
+	}
+
+}
